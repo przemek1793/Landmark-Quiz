f5a8f2ef1723c3f4768c41917eb3fe76d91c147a
Przemysław Pajestka <przemek1793@gmail.com>
2017-05-30 18:42:07 +0200

    endgame score

--- app/src/main/java/com/example/przemek/picturequiz/activities/NoQuestionsActivity.java
+++ app/src/main/java/com/example/przemek/picturequiz/activities/NoQuestionsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 import com.example.przemek.picturequiz.R;
 import com.example.przemek.picturequiz.database.LandmarkDatabaseHandler;
 
@@ -17,6 +18,9 @@ public class NoQuestionsActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_no_questions);
+        int score=getIntent().getExtras().getInt("score");
+        TextView scoretext=(TextView)findViewById(R.id.final_score);
+        scoretext.setText("Your final score is "+String.valueOf(score));
     }
 
     public void returnToMenu (View view)
